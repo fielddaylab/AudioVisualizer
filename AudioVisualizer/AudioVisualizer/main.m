a9d37fc14992442c559499a4fecb940622e9fe94
m1sterc001guy <jgmoeller@wisc.edu>
2013-06-20 12:31:01 -0500

    Initial Commit

---
+++ AudioVisualizer/AudioVisualizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AudioVisualizer
+//
+//  Created by Justin Moeller on 6/20/13.
+//  Copyright (c) 2013 Justin Moeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
